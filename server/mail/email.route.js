0b16b7d192aeeaab93994be2ce764859fc1b7294
Khôi <163515623+NguyenDuyKhoi98@users.noreply.github.com>
2025-03-05 23:30:20 +0700

    Delete server directory

--- server/mail/email.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const EmailController = require('./EmailController');
-
-// Route gửi email liên hệ
-router.post('/contact', EmailController.sendContactEmail);
-
-module.exports = router; 
--- server/mail/subscription.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SubscriptionController = require('./SubscriptionController');
-
-// Route đăng ký nhận tin
-router.post('/', SubscriptionController.subscribe);
-
-module.exports = router; 
